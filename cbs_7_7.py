854002d961645c2c4ab8f33a1c857130da752b7d
Anton Nechayevsky <shukhartrad@gmail.com>
2022-10-30 19:52:50 +0200

    Create a prototype of the "Personnel Accounting" program, in which it is possible to view and make changes to the structure (implement an interface (menu) with which you can manipulate data):
    last name:
    
        position: ...
    
        work experience: ...
    
        portfolio: ...
    
        efficiency ratio: ...
    
        technology stack: ...
    
        salary: ...
    
    Provide the ability to display sorting by last name and the most effective employee.

---
+++ cbs_7_7.py
@@ -0,0 +1,103 @@
+print('Welcome to our HR department!')
+workers = {
+    'Смирнов': {
+        'посада': 'менеджер з продажу',
+        'досвід роботи': 3,
+        'портфоліо': 'так',
+        'стек технологій': 'С++',
+        'зарплата': 1000,
+        'ефективність': 86
+    },
+    'Колягина': {
+        'посада': 'менеджер з продажу',
+        'досвід роботи': 3,
+        'портфоліо': 'так',
+        'стек технологій': 'С++',
+        'зарплата': 1000,
+        'ефективність': 69
+    },
+    'Костин': {
+        'посада': 'менеджер з продажу',
+        'досвід роботи': 3,
+        'портфоліо': 'так',
+        'стек технологій': 'С++',
+        'зарплата': 1000,
+        'ефективність': 78
+    },
+    'Щербаков': {
+        'посада': 'менеджер з продажу',
+        'досвід роботи': 3,
+        'портфоліо': 'так',
+        'стек технологій': 'С++',
+        'зарплата': 1000,
+        'ефективність': 91
+    },
+    'Борисова': {
+        'посада': 'менеджер з продажу',
+        'досвід роботи': 3,
+        'портфоліо': 'так',
+        'стек технологій': 'С++',
+        'зарплата': 1000,
+        'ефективність': 99
+    }
+}
+while True:
+    menu = input(
+        'Please select the action you wish to take: \n'
+        '1. View a list of employees \n'
+        '2. Edit employee data \n'
+        '3. Delete employee data \n'
+        '4. Add employee data \n'
+        '5. Finish work\n')
+    match menu:
+        case '1':
+            sorted_workers = dict(sorted(workers.items()))
+            for key, value in sorted_workers.items():
+                print(key)
+            print()
+        case '2':
+            worker = input('Please enter the last name of the employee: ')
+            if worker not in workers:
+                print('We do not have such an employee.')
+            else:
+                print(workers[worker])
+                option = input("Enter the parameter you want to change (посада, досвід роботи, портфоліо, коефіцієнт ефективності, стек технологій чи зарплата) or 'break' for menu: ")
+                if option not in workers[worker]:
+                    print('You entered the wrong parameter.')
+                else:
+                    value = input('Enter the value for the parameter: ')
+                    workers[worker][option] = value
+                    print(f'Employee {worker} data has been successfully edited. Updated employee data: ')
+                    print(workers[worker])
+        case '3':
+            worker = input('Please enter the last name of the employee for ELEMINATING!!! ')
+            if worker not in workers:
+                print('We do not have such an employee.')
+            else:
+                workers.pop(worker)
+                print(f'Employee {worker} ELEMINATED successfully')
+        case '4':
+            worker = input('Please enter the last name of the new employee: ')
+            position = input(f'Please enter position of {worker}: ')
+            exp = int(input(f'Please enter work experience of {worker} (integers only, please): '))
+            portfolio = input(f'Please enter portfolio existence of {worker} ("так" or "ні"): ')
+            tech = input(f'Please enter technology stack of {worker}: ')
+            salary = int(input(f'Please enter salary of {worker} (integers only, please): '))
+            effficiency = int(input(f'Please enter efficiency ratio of {worker} (integers only, please): '))
+            workers[worker] = {}
+            workers[worker]['посада'] = position
+            workers[worker]['досвід роботи'] = exp
+            workers[worker]['стек технологій'] = tech
+            workers[worker]['портфоліо'] = portfolio
+            workers[worker]['зарплата'] = salary
+            workers[worker]['ефективність'] = effficiency
+            print(f'Data {worker} entered successfully: {workers[worker]}')
+        case '5':
+            print('Have a nice day!')
+            break
+
+
+
+
+
+
