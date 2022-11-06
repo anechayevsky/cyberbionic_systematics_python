ae2ede19252a74412500845b6f8d02c5e6b47e41
Anton Nechayevsky <shukhartrad@gmail.com>
2022-11-06 10:59:55 +0200

    One string

--- cbs_9_3.py
+++ cbs_9_3.py
@@ -1,9 +1,5 @@
 def stairs(n):
-    if n == 1 or n == 2:
-        return 1
-    else:
-        return stairs(n - 1) + stairs(n - 2)
-
+    return 1 if (n == 1 or n == 2) else stairs(n - 1) + stairs(n - 2)
 
 def main():
     n = int(input('На яку сходинку бажаєта піднятися? '))
