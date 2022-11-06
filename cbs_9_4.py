d965c5334765dc049e2e6be63bd9593d04883703
Anton Nechayevsky <shukhartrad@gmail.com>
2022-11-06 10:57:51 +0200

    Write a recursive function that calculates the sum of positive integers in a given interval.

--- cbs_9_4.py
+++ cbs_9_4.py
@@ -1,9 +1,11 @@
 def diap_sum(start, stop):
     return start if start == stop else diap_sum(start + 1, stop) + start
 
+
 def main():
     a, b = map(int, input('Введіть начало та кінець діапазона через пробіл: ').split())
-    print(f'Сумма натуральних натуральних чисел, які входять до заданого проміжку, складає {diap_sum(a,b)}')
+    print(f'Сумма натуральних натуральних чисел, які входять до заданого проміжку, складає {diap_sum(a, b)}')
+
 
 if __name__ == '__main__':
     main()
